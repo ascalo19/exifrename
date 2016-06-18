f40aa57e2c63af687b4bd9a3754ae9c74b63960e
ascalo19 <ascalo19@ascalo19.net>
2016-06-19 00:54:26 +0200

    Added video support

--- src/main/java/exifrename/ExifRename.java
+++ src/main/java/exifrename/ExifRename.java
@@ -12,14 +12,16 @@ import org.apache.commons.logging.LogFactory;
 import java.io.File;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Rename one or more files using one of the EXIF dates found in it.
@@ -29,6 +31,15 @@ import java.util.List;
 public class ExifRename {
 
 	private static final Log log = LogFactory.getLog(ExifRename.class);
+
+	private static final Map<Pattern, DateFormat> regexDates = new HashMap<Pattern, DateFormat>();
+
+	static {
+		Locale en = new Locale("en_US");
+		regexDates.put(Pattern.compile("(\\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} \\d{4})"), new SimpleDateFormat("MMM dd HH:mm:ss yyyy", en));
+		regexDates.put(Pattern.compile("(\\d{4}:\\d{2}:\\d{2} \\d{2}:\\d{2}:\\d{2})"), new SimpleDateFormat("yyyy:MM:dd HH:mm:ss", en));
+		regexDates.put(Pattern.compile("(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}[-|+]\\d{4})"), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", en));
+	}
 
 	private List<String> pathList;
 	private boolean dryRun = false;
@@ -99,7 +110,13 @@ public class ExifRename {
 		try {
 			Date exifDate = getExifDate(file);
 			String fileExt = file.getName().substring(file.getName().lastIndexOf("."), file.getName().length());
-			String newFileName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(exifDate) + fileExt.toLowerCase();
+			String newBaseName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(exifDate);
+			if (file.getName().startsWith(newBaseName)) {
+				log.debug("File " + file.getPath() + " already renamed, skipping ...");
+				return;
+			}
+
+			String newFileName = newBaseName + fileExt.toLowerCase();
 			String newFilePath;
 			if (file.getParent() != null) {
 				newFilePath = file.getParent() + File.separator + newFileName;
@@ -117,14 +134,8 @@ public class ExifRename {
 						}
 					}
 				} else {
-					FileTime timestamp = Files.getLastModifiedTime(Paths.get(file.getAbsolutePath()));
-					String s = String.valueOf(timestamp.toMillis());
-					log.debug(" using millis " + s);
-					newFile = new File(newFilePath.substring(0, newFilePath.lastIndexOf(".")) + "_" + s.substring(s.length() - 3, s.length()) + fileExt.toLowerCase());
-					if (newFile.exists()) {
-						log.debug("File " + newFile.getPath() + " already exists, skipping ...");
-						return;
-					}
+					log.debug("File " + newFile.getPath() + " already exists, skipping ...");
+					return;
 				}
 			}
 
@@ -150,27 +161,26 @@ public class ExifRename {
 			metadata = ImageMetadataReader.readMetadata(file);
 		} catch (Exception e) {
 			log.debug("... no EXIF information found");
-			throw new Exception("No EXIF information found in file " + file.getPath(), e);
 		}
 
 		// Put back the real stdout
 		System.out.close();
 		System.setOut(out);
 
-		Class[] cats = {
-			ExifIFD0Directory.class,
-			ExifSubIFDDirectory.class,
-			ExifSubIFDDirectory.class,
-			GpsDirectory.class};
-
-		int[] tags = {
-			ExifIFD0Directory.TAG_DATETIME,
-			ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL,
-			ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED,
-			GpsDirectory.TAG_GPS_DATE_STAMP};
-
 		Date dateTime = null;
 		if (metadata != null) {
+			Class[] cats = {
+					ExifIFD0Directory.class,
+					ExifSubIFDDirectory.class,
+					ExifSubIFDDirectory.class,
+					GpsDirectory.class};
+
+			int[] tags = {
+					ExifIFD0Directory.TAG_DATETIME,
+					ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL,
+					ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED,
+					GpsDirectory.TAG_GPS_DATE_STAMP};
+
 			for (int i = 0; i < cats.length; i++) {
 				Directory directory = metadata.getDirectory(cats[i]);
 				for (int j = 0; j < tags.length; j++) {
@@ -188,6 +198,26 @@ public class ExifRename {
 			}
 		}
 
+		byte[] buffer = new byte[2048];
+		RandomAccessFile f = new RandomAccessFile(file, "r");
+		try {
+			f.read(buffer);
+			dateTime = getRegexDate(new String(buffer));
+			if (dateTime != null) {
+				return dateTime;
+			}
+			Arrays.fill(buffer, (byte) 0);
+			f.seek(f.length() - buffer.length);
+			f.read(buffer);
+			dateTime = getRegexDate(new String(buffer));
+			if (dateTime != null) {
+				return dateTime;
+			}
+		} finally {
+			f.close();
+		}
+
+
 		if (useLastModified) {
 			dateTime = new Date(file.lastModified());
 			log.debug("... used file's lastModified " + f(dateTime));
@@ -196,6 +226,19 @@ public class ExifRename {
 
 		log.debug("... no date found");
 		throw new Exception("No date found in file " + file.getPath());
+	}
+
+	private Date getRegexDate(String data) throws ParseException {
+		for (Pattern p : regexDates.keySet()) {
+			Matcher m = p.matcher(data);
+			if (m.find()) {
+				String match = m.group(0);
+				log.debug("... found " + match);
+				DateFormat f = regexDates.get(p);
+				return f.parse(match);
+			}
+		}
+		return null;
 	}
 
 	private String f(Date dateTime) {
